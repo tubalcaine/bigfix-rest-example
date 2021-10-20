1e6f83f3751d0f66db9c20b91e63517601030ea4
Michael Schwarz <mschwarz@multitool.net>
2021-10-19 20:48:00 -0500

    Fix use of parser (gotta save the args!)

--- src/runFixlet.py
+++ src/runFixlet.py
@@ -7,11 +7,11 @@ parser.add_argument("-p", "--bfport", type=int, help="BigFix Port number (defaul
 parser.add_argument("-U", "--bfuser", type=str, help="BigFix Console/REST User name")
 parser.add_argument("-P", "--bfpass", type=str, help="BigFix Console/REST Password")
 parser.add_argument("-m", "--match", type=str, help="Fixlet name pattern to match")
-parser.parse_args()
+conf = parser.parse_args()
 
-bf = bigfixREST.bigfixRESTConnection(parser.bfserver, parser.bfport, parser.bfuser, parser.bfpass)
+bf = bigfixREST.bigfixRESTConnection(conf.bfserver, conf.bfport, conf.bfuser, conf.bfpass)
 
-qres = bf.srQueryJson(f"(id of it, id of site of it | -1, content id of default action of it, concatenation \"~\" of names of applicable computers of it, name of it) of bes fixlets whose (exists applicable computer of it and exists default action of it and exists match (regex \"{parser.match}\") of name of it)")
+qres = bf.srQueryJson(f"(id of it, id of site of it | -1, content id of default action of it, concatenation \"~\" of names of applicable computers of it, name of it) of bes fixlets whose (exists applicable computer of it and exists default action of it and exists match (regex \"{conf.match}\") of name of it)")
 
 print(qres)
 
